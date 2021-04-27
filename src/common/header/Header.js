554636afc8f43940be42dec83950d8c1963f34c6
Sehbaz Rafik <sehbazm@gmail.com>
2021-04-27 17:44:00 +0100

    material-ui library added

---
+++ src/common/header/Header.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "../header/Header.css";
+class Header extends Component {
+  render() {
+    return <div>Hello from header</div>;
+  }
+}
+export default Header;
