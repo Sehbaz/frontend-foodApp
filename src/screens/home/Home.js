115ad43044ac9d5c8a8fac77fd6124faebe89bca
Sehbaz Rafik <sehbazm@gmail.com>
2021-04-28 03:39:14 +0100

    Testing API fetch

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,9 +1,43 @@
 import Header from "../../common/header/Header";
-
+import React, { useState, useEffect } from "react";
 export default function Home() {
-  return (
-    <div>
-      <Header />
-    </div>
-  );
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("https://mocki.io/v1/d4867d8b-b5d5-4a48-a4ab-79131b5809b8")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div>
+        <Header />
+        <ul>
+          {items.map((item) => (
+            <li key={item.id}>
+              {item.name} {item.price}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
