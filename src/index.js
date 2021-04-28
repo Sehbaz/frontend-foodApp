2b3de0ac0f92decbc7c113660a765e35450652cc
Sehbaz Rafik <sehbazm@gmail.com>
2021-04-28 02:33:08 +0100

    Header component under Home

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Header from "../src/common/header/Header";
+import Home from "./screens/home/Home";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Header />
+    <Home />
   </React.StrictMode>,
   document.getElementById("root")
 );
